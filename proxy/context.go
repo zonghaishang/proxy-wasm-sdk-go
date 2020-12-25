280cc5ae3e1c8386f57b1a10e9b490508b0f6612
zonghaishang <yiji@apache.org>
2020-12-25 16:25:19 +0800

    add protocol api.

--- proxy/context.go
+++ proxy/context.go
@@ -17,14 +17,68 @@ type RootContext interface {
 type FilterContext interface {
 	// OnDownStreamReceived Called when the data requests,
 	// The caller should check if the parameter value is nil
-	OnDownStreamReceived(headers Header, buffer Buffer, trailers Header) types.Action
+	OnDownStreamReceived(headers Header, data Buffer, trailers Header) types.Action
 	// OnUpstreamReceived Called when the data responds,
 	// The caller should check if the parameter value is nil
-	OnUpstreamReceived(headers Header, buffer Buffer, trailers Header) types.Action
+	OnUpstreamReceived(headers Header, data Buffer, trailers Header) types.Action
 	// Context Used to save and pass data during a session
 	Context() context.Context
 	OnFilterStreamDone()
 	OnLog()
+}
+
+// L7 layer extension
+type ProtocolContext interface {
+	Name() string         // protocol name
+	Codec() Codec         // frame encode & decode
+	KeepAlive() KeepAlive // protocol keep alive
+	Hijacker() Hijacker   // protocol hijacker
+}
+
+type Codec interface {
+	Decode(data Buffer) (Command, error)
+	Encode(message Command) (Buffer, error)
+}
+
+// Command base request or response command
+type Command interface {
+	// Header get the data exchange header, maybe return nil.
+	Header() Header
+	// GetData return the complete message byte buffer, including the protocol header
+	Data() Buffer
+	// SetData update the complete message byte buffer, including the protocol header
+	SetData(data Buffer)
+	// IsHeartbeat check if the request is a heartbeat request
+	IsHeartbeat() bool
+	// CommandId get command id
+	CommandId() uint64
+	// SetCommandId update command id
+	// In upstream, because of connection multiplexing,
+	// the id of downstream needs to be replaced with id of upstream
+	// blog: https://mosn.io/blog/posts/multi-protocol-deep-dive/#%E5%8D%8F%E8%AE%AE%E6%89%A9%E5%B1%95%E6%A1%86%E6%9E%B6
+	SetCommandId(id uint64)
+}
+
+type Request interface {
+	Command
+	// IsOneWay Check that the request does not care about the response
+	IsOneWay() bool
+	Timeout() uint32 // request timeout
+}
+
+type Response interface {
+	Command
+	Status() uint32 // response status
+}
+
+type KeepAlive interface {
+	KeepAlive(requestId uint64) Request
+	ReplyKeepAlive(request Request) Response
+}
+
+type Hijacker interface {
+	// Hijack allows sidecar to hijack requests
+	Hijack(request Request, code uint32) Response
 }
 
 // L4 layer extension (host not support now.)
@@ -38,14 +92,9 @@ type StreamContext interface {
 	OnLog()
 }
 
-type ProtocolContext interface {
-}
-
 type (
 	DefaultRootContext   struct{}
-	DefaultFilterContext struct {
-		ctx context.Context
-	}
+	DefaultFilterContext struct{ ctx context.Context }
 	DefaultStreamContext struct{}
 )
 
@@ -64,11 +113,11 @@ func (*DefaultRootContext) OnLog() {
 }
 
 // impl FilterContext
-func (c *DefaultFilterContext) OnDownStreamReceived(headers Header, buffer Buffer, trailers Header) types.Action {
+func (c *DefaultFilterContext) OnDownStreamReceived(headers Header, data Buffer, trailers Header) types.Action {
 	return types.ActionContinue
 }
 
-func (c *DefaultFilterContext) OnUpstreamReceived(headers Header, buffer Buffer, trailers Header) types.Action {
+func (c *DefaultFilterContext) OnUpstreamReceived(headers Header, data Buffer, trailers Header) types.Action {
 	return types.ActionContinue
 }
 
